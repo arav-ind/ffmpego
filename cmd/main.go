2cd6902b593756b2656b270199e6432b6f53ddd1
Aravind D <aravindrndv8@gmail.com>
2025-03-23 18:46:31 +0530

    feat: get user input

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/arav-ind/ffmpego/internal/converter"
 )
 
 func main() {
-	// Get the FFmpeg path
+	mp4Folder := flag.String("input", "", "Path to the folder containing MP4 files")
+	webmFolder := flag.String("output", "", "Path to the output folder for WebM files")
+
+	// Parse command-line arguments
+	flag.Parse()
+
+	// Ensure both flags are provided
+	if *mp4Folder == "" || *webmFolder == "" {
+		fmt.Println("Usage: ffmpego --input <input_folder> --output <output_folder>")
+		os.Exit(1)
+	}
+	
 	ffmpegPath, err := converter.GetFFmpegPath()
 	if err != nil {
 		log.Fatalf("Error: %v", err)
 	}
 
-	// Define input and output folders
-	inputFolder := "./mp4"
-	outputFolder := "./webm"
-
 	fmt.Println("Starting conversion process...")
 
 	// Convert files
-	if err := converter.ConvertToWebM(ffmpegPath, inputFolder, outputFolder); err != nil {
+	if err := converter.ConvertToWebM(ffmpegPath, *mp4Folder, *webmFolder); err != nil {
 		log.Fatalf("Error converting to WebM: %v", err)
 	}
 
